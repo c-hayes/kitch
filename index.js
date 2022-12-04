292075f2216456dab53fa058c658ee4eb8d05d3b
Chris hayes <chayes2913@gmail.com>
2022-12-04 12:29:52 -0600

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+            
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+// ADD in Payment processing
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        );
+      });
+  }
+
+let isModalOpen = false;
+function toggleModal(){
+    if (isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
